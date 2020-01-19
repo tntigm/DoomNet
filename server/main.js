c73d48019a9f2b5ce4af1d60364d8c4f66fecda4
tntigm <48128185+tntigm@users.noreply.github.com>
2020-01-19 20:26:23 +0100

    Update main.js

--- server/main.js
+++ server/main.js
@@ -11,6 +11,14 @@ var messages = [{
 
 app.use(express.static('public'));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 app.get('/hello', function(req, res) {
   res.status(200).send("Hello World!");
 });
