59f4bbab33eac70f5b3b5df6f2a74d2cde682075
tntigm <48128185+tntigm@users.noreply.github.com>
2020-01-19 19:19:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var express = require('express');
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 server.listen(8080, "0.0.0.0", function() {
   console.log("Server running @ http://" + "0.0.0.0" + ":" + 8080);
 });
