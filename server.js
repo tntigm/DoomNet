5f2ec0edd3556ae30ea8f7d7aca265788943970d
tntigm <48128185+tntigm@users.noreply.github.com>
2020-01-19 15:02:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+
+var ip = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
 server.listen(80, "testw3-doomnet.apps.us-east-2.starter.openshift-online.com", function() {
   console.log("Server running @ http://" + "testw3-doomnet.apps.us-east-2.starter.openshift-online.com" + ":" + 8080);
 });
