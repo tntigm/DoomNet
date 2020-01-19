d4376a885bd69c6211575fe24e2e797eca138f6a
tntigm <48128185+tntigm@users.noreply.github.com>
2020-01-19 15:56:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = require('express')();
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
 
 var ip = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
@@ -12,6 +11,7 @@ server.listen(port, ip, function() {
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+var io = require('socket.io')(server);
 io.on('connection', function (socket) {
 	console.log("New connection!");
   socket.emit('hello', { greeting: 'Hi socket ' + socket.id + ' this is Server speaking! Let\'s play ping-pong. You pass!' });
