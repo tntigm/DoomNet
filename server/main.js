8420708e6b3d53487e8d863349607e8d01079a5c
tntigm <48128185+tntigm@users.noreply.github.com>
2020-01-20 08:45:15 +0100

    Update main.js

--- server/main.js
+++ server/main.js
@@ -3,18 +3,11 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-var userOn={};
-
-io.origins(['https://sites.google.com/view/doomnet/userreg']);
-// Configurar cabeceras y cors
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
-
+var messages = [{
+  id: 1,
+  text: "Hola soy un mensaje",
+  author: "Carlos Azaustre"
+}];
 
 app.use(express.static('public'));
 
@@ -23,24 +16,16 @@ app.get('/hello', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-  console.log('New connection');
+  console.log('Alguien se ha conectado con Sockets');
+  socket.emit('messages', messages);
+
   socket.on('new-message', function(data) {
     messages.push(data);
 
     io.sockets.emit('messages', messages);
   });
-  
-  socket.on('new-user',(data)=>{
-    console.log(data);
-  });
-  socket.on('log-user',(data)=>{
-    
-  });
-  socket.on('send-msg',(data)=>{
-    
-  });
 });
 
 server.listen(8080, function() {
   console.log("Servidor corriendo en http://0.0.0.0:8080");
-});
+}); 
