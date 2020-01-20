f5ada74be58960771129c68fc6a0349c582d7b1f
tntigm <48128185+tntigm@users.noreply.github.com>
2020-01-20 16:32:51 +0100

    Update main.js

--- server/main.js
+++ server/main.js
@@ -8,7 +8,13 @@ var messages = [{
   text: "Hola soy un mensaje",
   author: "Carlos Azaustre"
 }];
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 app.use(express.static('public'));
 
 app.get('/hello', function(req, res) {
