c1c679724ab812d1d8da73038a985952104c45db
tntigm <48128185+tntigm@users.noreply.github.com>
2020-01-29 19:17:50 +0100

    Update main.js

--- server/main.js
+++ server/main.js
@@ -2,12 +2,25 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var mongoose = require('mongoose');
+
+function initDb(){
+  mongoose.connect('mongodb://0.0.0.0/doomnetDB', {useNewUrlParser: true});
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+    console.log(">>Connected to mongo succefully.");
+
+    var userSchema = new mongoose.Schema({name:String,password:String,friend:[{name:String}],state:Boolean});
+    var userComp = mongoose.model('User',userSchema);
+  });
+}
 
 
 
 
 //Variables
-var usersOn={};  //Diccionario con los sockets ip y nickname y varios datos.   name(String),ip(String),login(bool),state(bool)
+var usersOn={};  //Diccionario con los sockets ip y nickname y varios datos.   name(String),login(bool),state(bool)
 //Variables
 
 
@@ -33,6 +46,10 @@ app.get('/hello', function(req, res) {
 io.on('connection', function(socket) {
   console.log('Alguien se ha conectado con Sockets');
 
+  if(usersOn[socket.request.connection.remoteAddress]==null){
+  usersOn[socket.request.connection.remoteAddress]={name:"",state:false};
+  }
+
 //Uso en local  
   socket.on('new-message', function(data) {
     messages.push(data);
@@ -41,36 +58,75 @@ io.on('connection', function(socket) {
 });
 //Uso en local
 
-//Linkear sockets    
-io.on('link',(data)=>{
-    
-      
-});
   
 //Crear nuevo usuario  
 io.on('newUser',(data)=>{
+    User.findOne({"name":data.name},"name",(err,user)=>{
+      if (err){
+
+        io.to(usersOn[data.ip]).emit('accept',false);
+
+        return handleError(err);
+      }
+      if(user!=null){
+        var usr = new User({name:user.name,password:user.pass,friend:[],state:true}).save((err)=>{
+          if (err) throw err;
+          console.log("New user:"+user.name);
+        });
+   
+        io.to(usersOn[data.ip]).emit('accept',true);
+      }
+    });
     
-    
+
     
 });
   
   
 //Log usuario
 io.on('logIn',(data)=>{
+  User.findOne({"name":data.name},"name",(err,user)=>{
+    if (err){
 
+      io.to(usersOn[data.ip]).emit('accept',false);
+
+      return handleError(err);
+    }
+    if(user!=null){
+
+      console.log("Log In:"+user.name);
+      usersOn[data.ip].name = user.name;
+      usersOn[data,ip].state = true;
+      io.to(usersOn[data.ip]).emit('accept',true);
+
+    }
+  });
     
 });
   
 //Reenviar mensaje  
-io.on('recMsg',(data)=>{
+io.on('sendMsg',(data)=>{
 
+  io.to(usersOn[data.to]).emit('recvMsg',{from:data.from,msg:data.msg});
   
 });
   
 //Añadir amigo  
 io.on('addFriend',(data)=>{
-  
-    
+  User.findOne({"name":data.name},"name friend",(err,friends)=>{
+    if (err){
+
+
+      io.to(usersOn[data.ip]).emit('recvF',[]);
+
+      return handleError(err);
+    }
+    if(friends!=null){
+
+      io.to(usersOn[data.ip]).emit('recvF',friends);
+
+    }
+  });
 });
  
 //Acceder a settings
@@ -78,6 +134,24 @@ io.on('settings',(data)=>{
   
     
 });
+
+//Pedir los amigos que tiene
+io.on('getFriends',(data)=>{
+  User.findOne({"name":data.name},"name friend",(err,friends)=>{
+    if (err){
+
+      io.to(usersOn[data.ip]).emit('recvF',[]);
+
+      return handleError(err);
+    }
+    if(friends!=null){
+
+      io.to(usersOn[data.ip]).emit('recvF',friends);
+
+    }
+  });
+}
+);
 });
 
 //Iniciar mongoDB
@@ -89,4 +163,3 @@ console.log("Error:"+err);
 server.listen(8080, function() {
   console.log("Servidor corriendo en http://0.0.0.0:8080");
 });
-
