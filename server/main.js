ca9e8047eedb87f141ade070d3a064648d9aff14
tntigm <48128185+tntigm@users.noreply.github.com>
2020-02-24 18:31:14 +0100

    Update main.js

--- server/main.js
+++ server/main.js
@@ -72,7 +72,7 @@ socket.on("newUser",(data)=>{
         console.log("Error creating user.");*/
         if(user ==null || user ==undefined){
         
-          var usr = User.create({name:data.name,password:data.password,friend:[]}).save(function(er){
+          var usr = new User({name:data.name,password:data.password,friend:[]}).save(function(er){
             console.log(usr);
             if (er!=null || er!=undefined){console.log(er);}else{
             console.log("New user:"+data.name);
